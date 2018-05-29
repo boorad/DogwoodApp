7d915a1ca661c82ab33c76548b6363c6b04d0a23
Brad Anderson <brad@sankatygroup.com>
2018-05-29 16:50:29 -0400

    splash/launch screen for android

--- android/app/src/main/java/com/dogwood/MainActivity.java
+++ android/app/src/main/java/com/dogwood/MainActivity.java
@@ -1,6 +1,8 @@
 package com.dogwood;
 
 import com.facebook.react.ReactActivity;
+import org.devio.rn.splashscreen.SplashScreen;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +14,15 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Dogwood";
     }
+
+    /**
+     * Splash screen bits
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);  // here
+        super.onCreate(savedInstanceState);
+    }
+
 }
